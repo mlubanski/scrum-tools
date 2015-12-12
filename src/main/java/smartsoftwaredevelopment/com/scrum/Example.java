37bd2ef2dfc4f28fc099dba86f5671bb35283675
unknown <m.lubanskii@gmail.com>
2015-12-12 19:49:49 +0000

    create basic spring-boot project configuration

---
+++ src/main/java/smartsoftwaredevelopment/com/scrum/Example.java
@@ -0,0 +1,13 @@
+package smartsoftwaredevelopment.com.scrum;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Example {
+
+  @RequestMapping("/")
+  String home() {
+    return "Hello World!";
+  }
+}
